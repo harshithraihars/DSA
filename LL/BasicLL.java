dfacb3fb607409a3dd31dbc7efbfb030df264d9e
harshithraihars <harshithraiharsu@gmail.com>
2025-02-23 22:36:21 +0530

    ll basics done

--- LL/BasicLL.java
+++ LL/BasicLL.java
@@ -12,9 +12,10 @@ public class BasicLL {
 
     public static Node Head;
     public static Node Tail;
-
+    public static int size=0;
     public void addFirst(int data) {
         // create new node
+        size++;
         Node newNode = new Node(data);
         if (Head == null) {
             Head=Tail=newNode;
@@ -27,7 +28,9 @@ public class BasicLL {
         Head = newNode;
     }
     public void addLast(int data) {
+
         // create new node
+        size++;
         Node newNode = new Node(data);
         if (Head == null) {
             Head=Tail=newNode;
@@ -36,6 +39,45 @@ public class BasicLL {
         Tail.next=newNode;
         Tail=newNode;
     }
+
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("no elemnt present in ll");
+            return Integer.MAX_VALUE;
+        }
+        size--;
+        if(size==1){
+            int Value=Head.data;
+            Head=Tail=null;
+            return Value;
+        }
+        int Value=Head.data;
+        Head=Head.next;
+        return Value;
+    }
+
+    public int removeLast(){
+        if(size==0){
+            System.out.println("no elemnt present in ll");
+            return Integer.MAX_VALUE;
+        }
+        if(size==1){
+            int Value=Head.data;
+            Head=Tail=null;
+            size=0;
+            return Value;
+        }
+        Node temp=Head;
+        for(int i=0;i<size-2;i++){
+            temp=temp.next;
+        }
+        int Value=temp.next.data;
+        temp.next=null;
+        Tail=temp;
+        size--;
+        return Value;
+    }
+
     public void printLL() {
         // create new node
         Node temp=Head;
@@ -43,15 +85,48 @@ public class BasicLL {
             System.out.println("linked list is empty");return;
         }
         while (temp!=null) {
-            System.out.println(temp.data);
+            System.out.print(temp.data+" ");
             temp=temp.next;
         }
+        System.out.println("size is "+size);
+    }
+
+    public int IterativeSearch(int key) {
+        // create new node
+        Node temp=Head;
+        int i=0;
+        while (temp!=null) {
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return -1;
+    }
+    public int Helper(Node Head,int key){
+        if(Head==null){
+            return -1;
+        }
+        if(Head.data==key){
+            return 0;
+        }
+        int index=Helper(Head.next, key);
+        if(index==-1){
+            return -1;
+        }
+        return index+1;
+    }
+    public int RecursiveSearch(int key) {
+        // create new node
+        return Helper(Head,key);
     }
     public void addMiddle(int index,int data) {
         if(index==0){
             addFirst(data);
             return;
         }
+        size++;
         Node temp=Head;
         Node newNode=new Node(data);
         int i=0;
@@ -62,6 +137,8 @@ public class BasicLL {
         newNode.next=temp.next;
         temp.next=newNode;
     }
+
+
     public static void main(String[] args) {
         BasicLL ll=new BasicLL();
         ll.addLast(1);
@@ -70,5 +147,12 @@ public class BasicLL {
         ll.addLast(4);
         ll.addMiddle(1, 6);
         ll.printLL();
+        ll.removeFirst();
+        ll.printLL();
+        ll.removeLast();
+        ll.printLL();
+        System.out.println(ll.IterativeSearch(3));;
+        System.out.println(ll.RecursiveSearch(3));
+
     }
 }
