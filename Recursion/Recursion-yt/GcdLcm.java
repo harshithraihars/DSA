1b42499293607775daf7f8878571c2522564fda5
harshithraihars <harshithraiharsu@gmail.com>
2025-04-09 21:02:43 +0530

    recursion for pattern

--- Recursion/Recursion-yt/GcdLcm.java
+++ Recursion/Recursion-yt/GcdLcm.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class GcdLcm {
     public static int gcd(int a,int b){
         if(a==0){
@@ -36,11 +38,27 @@ public class GcdLcm {
         if(number%10==0) count+=1;
         return CountZeros(number/10, count);
     }
+
+    public static ArrayList<Integer> findMatchinGindex(int arr[],int num,int index){
+        ArrayList<Integer> ar=new ArrayList<>();
+        if(index==arr.length){
+            return ar;
+        }
+        if(arr[index]==num){
+            ar.add(index);
+        }
+
+        ArrayList<Integer> answerfromAboveCalls=findMatchinGindex(arr, num, index+1);
+        ar.addAll(answerfromAboveCalls);
+        return ar;
+    }
     public static void main(String[] args) {
         System.out.println(gcd(12, 18));
         System.out.println(findSumOfDigits(1342));
         System.out.println(prodOfDigits(555));
         System.out.println(RevreseNumber(123, 0));
         System.out.println(CountZeros(30204, 0));;
+        int arr[]={1,2,3,4,5,4,4,4};
+        System.out.println(findMatchinGindex(arr, 4, 0));
     }
 }
