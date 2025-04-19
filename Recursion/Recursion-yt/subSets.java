4b09d0fd031f815bf1c1d98b09672463c77242f7
harshithraihars <harshithraiharsu@gmail.com>
2025-04-19 11:43:04 +0530

    permtations in recursion

--- Recursion/Recursion-yt/subSets.java
+++ Recursion/Recursion-yt/subSets.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class subSets {
     public static ArrayList<String> subset(String str, String newstr) {
@@ -15,7 +16,46 @@ public class subSets {
         return ar1;
     }
 
+
+    public static List<List<Integer>> Iterattive(int[] arr){
+        List<List<Integer>> outer=new ArrayList<>();
+        outer.add(new ArrayList<Integer>());
+        for(int num:arr){
+            int n=outer.size();
+            for(int i=0;i<n;i++){
+                ArrayList<Integer> internal=new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    public static List<List<Integer>> DuplicateAvoidIterattive(int[] arr){
+        List<List<Integer>> outer=new ArrayList<>();
+        int start=0;
+        int end=0;
+        outer.add(new ArrayList<Integer>());
+        for(int i=0;i<arr.length;i++){
+            if(i>0 && arr[i]==arr[i-1]){
+                start=end+1;
+            }else{
+                start=0;
+            }
+            end=outer.size()-1;
+            int n=outer.size();
+            for(int j=start;j<n;j++){
+                ArrayList<Integer> internal=new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
     public static void main(String[] args) {
-        System.out.println(subset("abc", ""));
+        // System.out.println(subset("abc", ""));
+        int []arr={1,2,2,3,3};
+        System.out.println(DuplicateAvoidIterattive(arr));
+
     }
 }
