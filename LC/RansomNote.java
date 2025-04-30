c28cef33204e71861bbd468b3d7fc5877cfa39bc
harshithraihars <harshithraiharsu@gmail.com>
2025-04-30 20:44:52 +0530

    ransone note

---
+++ LC/RansomNote.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            if (--freq[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
