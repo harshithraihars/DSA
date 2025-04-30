9b442ff33742e3623c28ec72736a031a0c3801c2
harshithraihars <harshithraiharsu@gmail.com>
2025-04-30 10:04:26 +0530

    maze with obstacles

--- Recursion/Recursion-yt/MzeProblem.java
+++ Recursion/Recursion-yt/MzeProblem.java
@@ -26,7 +26,24 @@ public class MzeProblem {
         if(c!=1) Diag(r, c-1,s+"R");
         if(r!=1 && c!=1) Diag(r-1,c-1,s+"X");
     }
+
+    public static void BoardWithObstacles(int r,int c,String s,boolean[][] maze){
+        if(r==maze.length-1 && c==maze[0].length-1){
+            System.out.println(s);
+            return;
+        }
+        if(!maze[r][c]) return;
+        if(r!=maze.length-1) BoardWithObstacles(r+1, c,s+"D",maze);
+        if(c!=maze[0].length-1) BoardWithObstacles(r, c+1,s+"R",maze);
+        if(r!=maze.length-1 && c!=maze[0].length-1) BoardWithObstacles(r+1,c+1,s+"X",maze);
+    }
+
     public static void main(String[] args) {
-        Diag(3, 3,"");
+        boolean maze[][]={
+            {true,true,true},
+            {true,false,true},
+            {true,true,true}
+        };
+        BoardWithObstacles(0, 0,"", maze);
     }
 }
