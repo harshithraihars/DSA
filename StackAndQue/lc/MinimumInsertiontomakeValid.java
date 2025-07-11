2285fe66261cac25491b9f6bc8d6a4cee92ea1ee
harshithraihars <harshithraiharsu@gmail.com>
2025-07-11 16:16:55 +0530

    minimum Insertion to make a string valud

---
+++ StackAndQue/lc/MinimumInsertiontomakeValid.java
@@ -0,0 +1,20 @@
+package lc;
+
+public class MinimumInsertiontomakeValid {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        int min=0;
+        for(Character ch:s.toCharArray()){
+            if(ch==')'){
+                if(!stack.isEmpty() && stack.peek()=='('){
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }else{
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}
